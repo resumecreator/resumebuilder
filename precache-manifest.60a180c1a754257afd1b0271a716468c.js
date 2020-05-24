27960c6b6abacb06958c9c73856b2b6b59b223aa
Sebastian Velo <sebast.velo@gmail.com>
2020-05-24 11:36:25 -0300

    Updates

---
+++ precache-manifest.60a180c1a754257afd1b0271a716468c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f199cd498d993218852f22ed345c768",
+    "url": "/resumebuilder/index.html"
+  },
+  {
+    "revision": "497768744bc7cf55ebfe",
+    "url": "/resumebuilder/static/css/main.b1b9a13a.chunk.css"
+  },
+  {
+    "revision": "330256d2c08befc702d3",
+    "url": "/resumebuilder/static/js/2.00b867ac.chunk.js"
+  },
+  {
+    "revision": "c0cd05482c2c076b6ca3754f53f53494",
+    "url": "/resumebuilder/static/js/2.00b867ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "497768744bc7cf55ebfe",
+    "url": "/resumebuilder/static/js/main.255292c7.chunk.js"
+  },
+  {
+    "revision": "9d874e7c955c313edd04",
+    "url": "/resumebuilder/static/js/runtime-main.9181c912.js"
+  },
+  {
+    "revision": "58395202fadf1210342c6bf7a4fdfa8c",
+    "url": "/resumebuilder/static/media/template0.58395202.png"
+  },
+  {
+    "revision": "f0f9e719d95f48dbf34a472eb1bc2f0c",
+    "url": "/resumebuilder/static/media/template1.f0f9e719.png"
+  },
+  {
+    "revision": "1334055d0dbf916d2bc2acf898cb8c01",
+    "url": "/resumebuilder/static/media/template2.1334055d.png"
+  },
+  {
+    "revision": "532d1e40b38b1568cbc5f932267262c4",
+    "url": "/resumebuilder/static/media/template3.532d1e40.png"
+  }
+]);
